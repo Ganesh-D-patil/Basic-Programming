bd63e90cbb8a242f5b4ef65bed59d4b2f3bde61f
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-06-02 11:23:16 +0530

    Create primenumber.java

---
+++ primenumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class primenumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number : ");
+
+        int n = sc.nextInt();
+        while (n<=1) {
+            System.out.println("not prime");
+            return;
+        }
+       int count = 0;
+       for(int i=1; i<=n; i++){
+        if(n%i == 0){
+            count ++;
+        }
+       }
+       if(count == 2){
+        System.out.println("prime number");
+       }else{
+        System.out.println("not prime");
+       }
+    }
+}
