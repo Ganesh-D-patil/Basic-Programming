72d619fcf0c31ad572358772e979fac6e30a1ab2
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-06-02 10:48:04 +0530

    Create evenOdd.java

---
+++ evenOdd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your number : ");
+        int n = sc.nextInt();
+
+    while (n != 1) {
+        System.out.println(n+ " ");
+        
+        if(n%2 ==0){
+            n = n/2;
+        }else{
+            n = n*3+1;
+        }
+    }
+    System.out.println("1");
+    }
+    
+}
