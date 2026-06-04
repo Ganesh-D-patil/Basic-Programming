9c38550abcf4b24e35573d6a0cfe324e4b688763
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-06-04 14:36:09 +0530

    Create palindromes.java

---
+++ palindromes.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class palindromes {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("This is a palindrome");
+        } else {
+            System.out.println("This is not a palindrome");
+        }
+
+        sc.close();
+    }
+}
