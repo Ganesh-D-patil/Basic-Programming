e45406c19c2e265880b2a8d21504e156288cf2d5
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-06-21 18:26:58 +0530

    Create SumOfElement.java

---
+++ SumOfElement.java
@@ -0,0 +1,18 @@
+public class SumOfElement {
+
+    public static void main(String[] args) {
+        System.out.println(" enter the number: ");
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+
+        System.out.println("Sum of elements = " + sum);
+
+
+    }
+}
