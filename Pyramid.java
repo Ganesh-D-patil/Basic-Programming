af9128ea00f520840224bd1363888862703f6cfe
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-06-26 09:46:51 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -7,7 +7,7 @@ public class Pyramid {
             System.out.print(" ");
             }
             for(int j =1; j<=2*i-1; j++){
-                System.out.print("*");
+                System.out.print("");
             }
             System.out.println();
         }
