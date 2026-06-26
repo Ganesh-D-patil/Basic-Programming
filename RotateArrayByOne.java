ae96af627397c2b3513dbc3afefbe891f1aa2351
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-06-26 09:46:59 +0530

    Create RotateArrayByOne.java

---
+++ RotateArrayByOne.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RotateArrayByOne {
+
+    public static void main(String args[]) {
+
+        int arr[] = {1, 2, 3, 4, 5};
+
+        int n = arr.length;
+
+        int temp = arr[0];
+
+        for(int i = 1; i < n; i++) {
+            arr[i - 1] = arr[i];
+        }
+
+        arr[n - 1] = temp;
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
