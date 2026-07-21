a696cff585d8e383df945c374a6f9745c5e649d5
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-07-21 17:40:54 +0530

    Create TriangleOfZeroOne.java

---
+++ TriangleOfZeroOne.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class TriangleOfZeroOne {
+
+    public void drawTriangleOfZeroOne(int n) {
+        int start = 1;
+        for(int i = 0; i<n; i++){
+            if(i%2==0)
+                start = 0;
+            else
+                start = 1;
+            for(int j = 0; j<=i; j++){
+                System.out.print(start + " ");
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the triangle: ");
+        int n = sc.nextInt();
+        TriangleOfZeroOne obj = new TriangleOfZeroOne();
+        obj.drawTriangleOfZeroOne(n);
+        sc.close();
+    }
+}
