0c6ed29df783299abd4cb9051e00600223b8122b
Ganesh Patil <144130668+Ganesh-D-patil@users.noreply.github.com>
2026-08-02 15:38:51 +0530

    Create PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,23 @@
+public class PalindromeString {
+
+    public static void main(String[] args) {
+
+        String str = "GaneshG";
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                System.out.println("Not Palindrome");
+                return;
+            }
+
+            left++;
+            right--;
+        }
+
+        System.out.println("Palindrome");
+    }
+}
